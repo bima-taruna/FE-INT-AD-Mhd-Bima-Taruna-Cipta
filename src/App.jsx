918c23575a61c628487fb2aad596fc454d97a855
bima-taruna <bimataruna21@gmail.com>
2024-09-15 09:21:22 +0700

    membuat component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import NavBar from "./component/Navbar";
+
 function App() {
   return (
     <>
+      <NavBar />
       <div className="text-3xl font-bold underline">heloooo</div>
     </>
   );
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div className="bg-blue-400 w-screen py-4 px-6 md:py-8 md:px-8 ">
+      <h5 className="font-bold">Simple To Do</h5>
+    </div>
+  );
+}
+
+export default NavBar;
