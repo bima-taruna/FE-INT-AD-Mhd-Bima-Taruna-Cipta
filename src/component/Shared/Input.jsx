b2f0f15fe8a3cc4a9c7a36ab9affc9bafbf83c36
bima-taruna <bimataruna21@gmail.com>
2024-09-16 08:30:04 +0700

    <refactor>(input) : mengambil text content dari props

--- src/component/Shared/Input.jsx
+++ src/component/Shared/Input.jsx
@@ -5,13 +5,13 @@ function Input({ name, type, placeholder, isRequired, onChange }) {
         htmlFor={name}
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
-        Email :
+        {name.charAt(0).toUpperCase() + name.slice(1)} :
       </label>
       <input
         type={type}
         name={name}
         id={name}
-        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         required={isRequired}
         onChange={onChange}
