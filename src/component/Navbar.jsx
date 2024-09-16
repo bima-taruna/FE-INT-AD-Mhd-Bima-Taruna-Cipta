0fc4758ebd2f23ca6e466a12007cec1085d6e529
bima-taruna <bimataruna21@gmail.com>
2024-09-16 11:09:44 +0700

    <style>(Navbar) : membuat navbar fixed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 function NavBar() {
   return (
-    <div className="bg-blue-400 w-screen py-4 px-6 md:py-8 md:px-8 ">
+    <div className="fixed top-0 left-0 bg-blue-400 w-screen py-4 px-6 md:py-8 md:px-8 ">
       <h5 className="font-bold">Simple To Do</h5>
     </div>
   );
