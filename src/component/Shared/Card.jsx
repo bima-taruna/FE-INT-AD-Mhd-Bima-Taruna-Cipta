dbf34ca6792e6fbef36fd2ffa0641880e90e2856
bima-taruna <bimataruna21@gmail.com>
2024-09-16 13:22:48 +0700

    menambah props untuk styling

--- src/component/Shared/Card.jsx
+++ src/component/Shared/Card.jsx
@@ -1,6 +1,8 @@
-function Card({ children }) {
+function Card({ wide = "w-full", children, border }) {
   return (
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <div
+      className={`w-full ${wide} ${border} relative flex flex-col bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700`}
+    >
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">{children}</div>
     </div>
   );
