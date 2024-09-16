1e6dc3ab1bb6afef88f7e8743c9d3ad124ba3c78
bima-taruna <bimataruna21@gmail.com>
2024-09-16 13:40:29 +0700

    <feature>(App) : menambahkan fungsi logout dan auto logout apabila tidak ada token di storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect } from "react";
 import NavBar from "./component/Navbar";
 import TaskList from "./component/TaskList";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "./helper/AxiosInstance";
 
 function App() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      navigate("/", {
+        replace: true,
+      });
+    }
+  }, [navigate, token]);
+
+  const handleLogout = async () => [
+    await axiosInstance.post("/logout").then(() => {
+      localStorage.removeItem("token");
+      navigate("/", {
+        replace: true,
+      });
+    }),
+  ];
+
   return (
     <>
-      <NavBar />
+      <NavBar onClick={handleLogout} />
       <TaskList />
     </>
   );
