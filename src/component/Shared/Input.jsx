714b297ae183fb3fb942734688619ae9dc707403
bima-taruna <bimataruna21@gmail.com>
2024-09-16 07:27:37 +0700

    membuat komponen input

---
+++ src/component/Shared/Input.jsx
@@ -0,0 +1,23 @@
+function Input({ name, type, placeholder, isRequired, onChange }) {
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        Email :
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder={placeholder}
+        required={isRequired}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default Input;
