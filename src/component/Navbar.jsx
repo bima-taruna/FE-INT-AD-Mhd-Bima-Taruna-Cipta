dff4bcb08198875f899f1c74cd6619a1c97a2519
bima-taruna <bimataruna21@gmail.com>
2024-09-16 13:38:46 +0700

    menambahkan tombol logout

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,8 @@
-function NavBar() {
+function NavBar({ onClick }) {
   return (
-    <div className="fixed top-0 left-0 bg-blue-400 w-screen py-4 px-6 md:py-8 md:px-8 ">
+    <div className="fixed flex justify-between items-center top-0 left-0 bg-blue-400 w-screen py-4 px-6 md:py-8 md:px-8 ">
       <h5 className="font-bold">Simple To Do</h5>
+      <button onClick={onClick}>Log out</button>
     </div>
   );
 }
